07b343b9317e4c93633046e97425cfc212111402
Mamie Wang <szmamie@pennmedicine.upenn.edu>
2019-03-15 21:09:38 -0400

    Added maximum clade probability tree

--- R/visualization.R
+++ R/visualization.R
@@ -46,3 +46,45 @@ plot_LOSO_performance <- function(performances) {
                          filter(metrics == 'MCV')) + xlab("Mean coverage")
   return(list(p_MRE = p_MRE, p_MRW = p_MRW, p_MCV = p_MCV))
 }
+
+
+plot_trajectory <- function(df, estimated_root_height = NULL) {
+  xmin <- c()
+  xmax <- c()
+  year <- floor(min(df$Time))
+  while (year < max(df$Time)) {
+    xmin <- c(xmin, year + 0.9)
+    xmax <- c(xmax, year + 1.2)
+    year <- year + 1
+  }
+  winter_seasons <- data.frame(xmin = xmin, xmax = xmax) %>%
+    filter(xmin < max(df$Time) & xmax > min(df$Time))
+  p <- ggplot(data = df) +
+    geom_rect(data = winter_seasons, aes(xmin = xmin, xmax = xmax, ymin = min(df$Lower), ymax = max(df$Upper)), fill = 'steelblue', alpha = 0.1) +
+    geom_line(aes(x = Time, y = Median), size = 1) +
+    geom_line(aes(x = Time, y = Upper), size = 0.5) +
+    geom_line(aes(x = Time, y = Lower), size = 0.5) +
+    scale_y_log10() +
+    theme_classic() +
+    ylab(expression(N["e"])) +
+    theme(axis.text.x = element_text(size = 13), axis.title = element_blank())
+
+  if(!is.null(estimated_root_height)) {
+    p <- p + geom_vline(aes(xintercept = estimated_root_height), linetype = "dotted")
+  }
+  return(p)
+}
+
+plot_MCC_Ne <- function(MCC, root_time, last_time, Ne, breaks) {
+  par(mar = c(2, 3.8, 0, 1))
+  layout(matrix(c(1, 2), ncol = 1), c(4, 4), c(1.2, 1))
+  plot(ape::ladderize(MCC), show.tip.label=F, show.node.label=F)
+  ape::axisPhylo(root.time = root_time, backward = F)
+  plot.new()
+  vps <- baseViewports()
+  pushViewport(vps$figure)
+  vp1 <-plotViewport(c(1,0.1,0,0.5))
+  p <- plot_trajectory(Ne) +
+    scale_x_continuous(limits = c(root_time, last_time), breaks = breaks)
+  print(p, vp = vp1)
+}
--- data-raw/fasta_qc.R
+++ data-raw/fasta_qc.R
@@ -1,5 +1,54 @@
 library(ggplot2)
 library(dplyr)
+library(gridBase)
+library(grid)
+
+preprocess_fasta <- function(path) {
+  fasta_file <- readLines(path)
+  header_lines <- which(grepl("^>", fasta_file, fixed = F))
+  headers <- fasta_file[header_lines]
+  fields <- purrr::map(headers, ~as.data.frame(matrix(strsplit(gsub("_|>", " ", .x), "|", fixed = T)[[1]], nrow = 1)))
+  fields <- dplyr::bind_rows(fields)
+  colnames(fields) <- c("isolate_name", "collection_date", "last_modified",
+                        "type", "lineage", "originating lab", "submitting lab")
+  fields$isolate_name <- gsub(" ", "", fields$isolate_name)
+  fields$collection_date <- as.POSIXct(strptime(gsub(" ", "", fields$collection_date),
+                                                format = "%Y-%m-%d"))
+  fields$last_modified <- as.POSIXct(strptime(gsub(" ", "", fields$last_modified),
+                                              format = "%Y-%m-%d"))
+  fields$type <- gsub(" ", "", fields$type)
+  fields$lineage <- gsub(" ", "", fields$lineage)
+  fields$id <- seq(nrow(fields))
+  fields <- fields %>%
+    arrange(isolate_name, desc(last_modified)) %>%
+    mutate(duplicate = duplicated(isolate_name)) %>%
+    arrange(id)
+  return(list(fasta_file = fasta_file, fields = fields))
+}
+
+filter_date <- function(fasta, start_date, field_outpath, data_output) {
+  fasta_file <- fasta$fasta_file
+  fields <- fasta$fields
+  line_ranges <- data.frame(id = seq_along(header_lines), start = header_lines)
+  line_ranges$end <- c(line_ranges$start[-1] - 1, length(fasta_file))
+  remove_isolates <- which(is.na(fields$collection_date) | fields$duplicate | fields$collection_date < as.POSIXct(start_date))
+  remove_ranges <- line_ranges[remove_isolates,]
+  remove_ranges$remove_idx <- with(remove_ranges, purrr::map2(start, end, ~ seq(.x, .y)))
+  fasta_filtered <- fasta_file[-unlist(remove_ranges$remove_idx)]
+  fields_filtered <- fields[-remove_isolates,]
+  readr::write_tsv(fields_filtered, path = field_outpath)
+  filtered_headers_idx <- which(grepl(">", fasta_filtered))
+  filtered_headers <- fasta_filtered[filtered_headers_idx]
+  fasta_filtered[filtered_headers_idx] <- paste0(">", fields_filtered$isolate_name, "|", fields_filtered$collection_date)
+  writeLines(fasta_filtered, data_output)
+  fields_filtered$collection_date <- as.POSIXct(strptime(fields_filtered$collection_date,
+                                                         format = "%Y-%m-%d"))
+  return(list(fasta_file = fasta_filtered, fields = fields_filtered))
+}
+
+# H3
+Yamagata <- preprocess_fasta("~/Downloads/20190315_NewYork_B_Yamagata.fasta")
+
 
 # get the header lines
 fasta_file <- readLines("inst/extdata/NewYork_A_H3N2_20190307.fasta")
@@ -43,3 +92,25 @@ fields_filtered$collection_date <- as.POSIXct(strptime(fields_filtered$collectio
 p <- ggplot(data = fields_filtered) +
   geom_histogram(aes(x = collection_date), bins = 100)
 ggsave(p, file = "~/Desktop/H3N2_NewYork.png", width = 5, height = 4)
+
+# read the tree after running the BEAST
+H3N2_MCC_tree <- ape::read.nexus("data-raw/20190308_H3N2_SkyGrid/MCC.tree")
+H3N2_MCC_phylo <- phylodyn::summarize_phylo(H3N2_MCC_tree)
+last_time <- 2019.0739726027398
+root_time = last_time - max(H3N2_MCC_phylo$coal_times)
+estimated_root_height <- last_time - 6.687
+
+# read output from skygrid reconstruction
+
+skygrid <- readr::read_tsv("data-raw/20190308_H3N2_SkyGrid/skygrid_reconstruction.tsv", skip = 1) %>%
+  mutate(Time = last_time - Time)
+
+pdf(file = "~/Desktop/H3N2.pdf", width = 5, height = 6)
+plot_MCC_Ne(H3N2_MCC_tree, root_time, last_time, skygrid, breaks = seq(2013, 2019))
+dev.off()
+
+# ILI index
+ny_data <- cdcfluview::ilinet(region = "state") %>%
+  filter(region == 'New York')
+cdc_data <- select(ny_data, year, week, ilitotal)
+cdc_data$Time <-
